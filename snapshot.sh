bc103a4e55e5c4df9240e8fbe13f29e7cbe16ba9
Krish <24amtics161@gmail.com>
2026-05-31 10:46:28 +0000

    Used bash variables to automate little things

--- snapshot.sh
+++ snapshot.sh
@@ -1,9 +1,23 @@
 mkdir -p logs
+logfile="logs/snapshot.log"
+date=$(date)
+user_name=$(whoami)
+dir=$(pwd)
+os=$(uname)
+
 echo "=== System Snapshot ===" > logs/snapshot.log
-date >> logs/snapshot.log
-whoami >> logs/snapshot.log
-id >> logs/snapshot.log
-uname >> logs/snapshot.log
-pwd >> logs/snapshot.log
-ls -la ~  >> logs/snapshot.log
+
+echo "Today is: $date" >> $logfile
+
+echo "Your username is: $user_name" >> $logfile
+
+id >> $logfile
+
+echo "Your Operating system is: $os" >> $logfile
+
+echo "Your current directory is: $dir" >> $logfile
+
+ls -la ~ >> $logfile
+
 echo "Snapshot successfully saved to logs/snapshot.log"
+
